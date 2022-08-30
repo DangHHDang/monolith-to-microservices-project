0a6afa7e425b55c72b2a80c539998317e848196c
dangdhh <dangdhh.mash@gmail.com>
2022-08-30 15:33:23 +0700

    first commit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a15274dc8246c4c52b5afb312e97b0d2-1899029883.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
